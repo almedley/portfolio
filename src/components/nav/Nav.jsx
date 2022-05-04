c04db63bf918307da8e7142ecde44d9d9f46bdb9
almedley <annie.medley@yahoo.com>
2022-05-04 16:47:47 -0400

    create basic nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,8 +3,18 @@ import './Nav.css';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div class="nav_title">
+        <h2>Annie Medley / Software Engineer</h2>
+      </div>
+      <div class="nav_links">
+        <a href="#about">About Me</a>
+        <a href="#resume">Resume</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
   )
 }
 
-export default Nav
+export default Nav;
