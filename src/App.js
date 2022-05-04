64283072fea5b56ca59da051fd99a08301075513
almedley <annie.medley@yahoo.com>
2022-05-04 14:49:10 -0400

    basic project setup with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Landing from './components/landing/Landing';
+import Nav from './components/nav/Nav';
+import Projects from './components/projects/Projects';
+import Resume from './components/resume/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Nav />
+      <Landing />
+      <About />
+      <Resume />
+      <Projects />
+      <Contact />
+    </Fragment>
   );
 }
 
